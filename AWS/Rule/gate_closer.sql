f2b7f6ca84e26ddac0e51b8d6e7730b9b9049db7
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2025-06-13 20:57:53 -0400

    add aws message routing and functions

---
+++ AWS/Rule/gate_closer.sql
@@ -0,0 +1,11 @@
+SELECT
+topic(3) AS thing_name,
+substring(topic(3),0, 10) AS thing_type,
+substring(topic(3),11, 15) AS thing_purpose,
+substring(topic(3), 16, 20) AS serial_number,
+timestamp() AS event_time
+FROM 
+'$aws/things/+/shadow/update'
+WHERE 
+state.reported.counter_sensor="BLOCKED" AND
+state.reported.gate_actuator="OPEN"
